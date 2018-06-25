67720b875a9b033f73b26ae6e5fd6791f06751e2
Max <alfonsomaxhernandez@gmail.com>
2018-06-25 02:11:20 -0700

    Integrated FullScreenImage component for full screen view of image when clicked

--- photo-gallery/src/Components/PhotoFrame3.js
+++ photo-gallery/src/Components/PhotoFrame3.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import FullScreenImage from './FullScreenImage';
 import './photoframe3.css';
 
 class PhotoFrame3 extends Component {
+  constructor(){
+      super();
+      this.state = {
+        showFullScreen: false
+      };
+  }
+
+  toggleFullScreen() {
+    console.log("Togggle");
+    this.setState({
+      showFullScreen: !this.state.showFullScreen
+    });
+  }
+
   render() {
     return (
       <div className="photo-container-third">
-        <img src={this.props.imgUri} />
+        <div className="photo">
+        <img
+          onClick={this.toggleFullScreen.bind(this)}
+          src={this.props.imgUri}
+        />
+        </div>
+        {this.state.showFullScreen ?
+            <FullScreenImage
+              imgUri={this.props.imgUri}
+              closeFullScreen={this.toggleFullScreen.bind(this)}
+            />
+            : null
+        }
       </div>
     );
   }
